1efeb6dab80da0772627141a965460f79e7dd989
Mahadi Hassan <itsmahadi@gmail.com>
2020-12-07 17:01:24 +0600

    Add pet  and vaccine tables in Model

--- wisdompets/adoptions/models.py
+++ wisdompets/adoptions/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Pet(models.Model):
+    SEX_CHOICES = [('M','Male'),('F','Female')]
+    name = models.CharField(max_length=100)
+    submitter = models.CharField(max_length=100)
+    species = models.CharField(max_length=30, blank=True)
+    description = models.TextField()
+    sex = models.CharField(max_length=1,choices=SEX_CHOICES,blank=True)
+    submission_date = models.TextField()
+    age = models.IntegerField(null=True)
+    vaccinations = models.ManyToManyField('Vaccine',blanl=True)
+
+class Vaccine(models.Model):
+    name = models.CharField(max_length=50)
